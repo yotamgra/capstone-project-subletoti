6eac6ca0b48c7f91a91e1d06e583285f75d2f3ad
Yotam Granada <yotam.granada@gmail.com>
2023-01-16 17:27:41 +0200

    Create getPost, setPost, updatePost and deletPosts functions in postController and conect them to postsRoutes

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,23 +1,63 @@
 import asyncHandler from "express-async-handler";
+import Post from "../model/postModel.js";
 
 //@desc    Get posts
 //@route   GET /posts
 //@access  Private
 const getPosts = asyncHandler(async (req, res) => {
-    res.status(200).send({ message: "Get posts" });
-  });
-
+  const posts = await Post.find();
+  res.status(200).send(posts);
+});
 
 //@desc    Set post
 //@route   POST /posts
 //@access  Private
 const setPost = asyncHandler(async (req, res) => {
-    if(!req.body.text){
-      res.status(400)
-      throw new Error("please add a text field")
-    }
-  
-    res.status(200).send({ message: "Set post" });
+  const { header, price, location, description, img } = req.body.post;
+  if (!header || !price || !location) {
+    res.status(400);
+    throw new Error("Post header, price and location are required");
+  }
+  const post = await Post.create({
+    header,
+    price,
+    location,
+    description: description || "",
+    img: img || "",
   });
+  res.status(200).json(post);
+});
 
-export { getPosts, setPost };
+//@desc    Update post
+//@route   PUT /posts/:id
+//@access  Private
+const updatePost = asyncHandler(async (req, res) => {
+  console.log(req.body);
+  const post = await Post.findById(req.params.id);
+  if (!post) {
+    res.status(400);
+    throw new Error("Post not found");
+  }
+
+  const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedPost);
+});
+
+//@desc    Delete post
+//@route   DELETE /posts/:id
+//@access  Private
+const deletePost = asyncHandler(async (req, res) => {
+  const post = await Post.findById(req.params.id);
+
+  if (!post) {
+    res.status(400);
+    throw new Error("Post not found");
+  }
+
+  await post.remove();
+  res.status(200).json({ id: req.params.id });
+});
+
+export { getPosts, setPost, updatePost, deletePost };
--- backend/routes/postsRoutes.js
+++ backend/routes/postsRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getPosts, setPost } from "../controllers/postController.js";
+import { deletePost, getPosts, setPost, updatePost } from "../controllers/postController.js";
 
 const router = express.Router();
 
 router.route("/").get(getPosts).post(setPost)
+
+router.route("/:id").put(updatePost).delete(deletePost)
 
 export { router as postsRoutes };
