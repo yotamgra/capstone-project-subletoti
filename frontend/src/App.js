dd8a593cbb91fc23bd60218f9371eab99401104b
Yotam Granada <yotam.granada@gmail.com>
2023-01-16 23:57:43 +0200

    Initial frontend folder

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function App() {
+  return (
+    <>
+     <Router>
+    <div>
+    <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+    </div>
+    </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,9 @@
+
+
+function Register() {
+  return (
+    <div>Register</div>
+  )
+}
+
+export default Register
