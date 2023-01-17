e7080f8fb0d3684984bdea3e96ccbfb8fe94f834
Yotam Granada <yotam.granada@gmail.com>
2023-01-17 18:19:16 +0200

    Create authSlice file and add auth reducer to store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from '../features/auth/authSlice.js'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer
+  },
 });
